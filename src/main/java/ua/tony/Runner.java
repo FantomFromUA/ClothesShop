df56b5539810a1631fd7cba8ab261abb4da93e8a
Erik <erik.toroni@gmail.com>
2022-10-06 12:33:39 +0200

    first commit

---
+++ src/main/java/ua/tony/Runner.java
@@ -0,0 +1,21 @@
+package ua.tony;
+
+import org.springframework.boot.CommandLineRunner;  
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Runner implements CommandLineRunner {
+    
+
+    public static void main(String[] args) {
+    	SpringApplication.run(Runner.class, args);
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
