00b2ca107c5a778a25d4dff22d122abef61e6a48
Timur Turianytsia <turianytsia.timur@gmail.com>
2022-10-13 13:13:18 +0300

    Added services and Fixed Bugs

--- src/main/java/ua/tony/Runner.java
+++ src/main/java/ua/tony/Runner.java
@@ -1,13 +1,10 @@
 package ua.tony;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import ua.tony.entity.Product;
 import ua.tony.mapper.OrderItemMapper;
 import ua.tony.mapper.OrderMapper;
 import ua.tony.mapper.ProductMapper;
@@ -16,37 +13,57 @@ import ua.tony.repository.OrderItemRepository;
 import ua.tony.repository.OrderRepository;
 import ua.tony.repository.ProductRepository;
 import ua.tony.repository.UserRepository;
+import ua.tony.service.OrderItemService;
+import ua.tony.service.OrderService;
+import ua.tony.service.ProductService;
+import ua.tony.service.UserService;
 
 @SpringBootApplication
 public class Runner implements CommandLineRunner {
 
-    @Autowired
-    private ProductRepository productRepository;
+	@Autowired
+	private ProductRepository productRepository;
 
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-    @Autowired
-    private OrderRepository orderRepository;
+	@Autowired
+	private OrderRepository orderRepository;
 
-    @Autowired
-    private OrderItemRepository orderItemRepository;
-    @Autowired
-    private ProductMapper productMapper;
-    @Autowired
-    private UserMapper userMapper;
-    @Autowired
-    private OrderMapper orderMapper;
-    @Autowired
-    private OrderItemMapper orderItemMapper;
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	
+	@Autowired
+	private ProductMapper productMapper;
+	
+	@Autowired
+	private UserMapper userMapper;
+	
+	@Autowired
+	private OrderMapper orderMapper;
+	
+	@Autowired
+	private OrderItemMapper orderItemMapper;
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private OrderService orderService;
+	
+	@Autowired
+	private OrderItemService orderItemService;
+	
+	@Autowired
+	private ProductService productService;
 
-    public static void main(String[] args) {
-	SpringApplication.run(Runner.class, args);
+	public static void main(String[] args) {
+		SpringApplication.run(Runner.class, args);
 
-    }
+	}
 
-    @Override
-    public void run(String... args) throws Exception {
+	@Override
+	public void run(String... args) throws Exception {
 
 ////    	USER CREATION
 //    	User user1 = new User("Ivan", "Ivanov", "ivan228", "ivanTheBest", 0, false);
@@ -90,35 +107,8 @@ public class Runner implements CommandLineRunner {
 //    	orderItemRepository.deleteById(1);
 
 ////		SOME SHIT
-//    	Order order = orderRepository.findById(6).get();
-//    	System.out.println(order.getOrderItems().size());
-
-	/*
-	 * Product product=productRepository.findById(1).get();
-	 * 
-	 * ProductDto productDto=productMapper.convertToDto(product); Product
-	 * product1=productMapper.convertToEntity(productDto);
-	 * System.out.println(product1.toString());
-	 */
-	/*
-	 * Product product = productRepository.findById(1).get(); User
-	 * user=userRepository.findById(3).get(); Order
-	 * order=orderRepository.findById(4).get(); OrderItem orderItem=new OrderItem();
-	 * orderItem.setProduct(product); orderItem.setOrder(order);
-	 * orderItemRepository.save(orderItem);
-	 */
-	/*User user = userRepository.findById(3).get();
-	UserDto urerDto= userMapper.convertToDto(user);
-	User user1=userMapper.convertToEntity(urerDto);
-	Order order= orderRepository.findById(4).get();
-        OrderDto orderDto=orderMapper.convertToDto(order);*/
-	//List<Order> orders= orderRepository.getOrdersThatRelatedToUser(3);
-//	List<OrderItem> orderItems=orderItemRepository.getOrderItemsThatRelatedToOrder(4);
-	/*OrderItem orderItem=orderItemRepository.findById(2).get();
-	OrderItemDto orderItemDto=orderItemMapper.convertToDto(orderItem);
-	OrderItem orderItem1=orderItemMapper.convertToEntity(orderItemDto);*/
-	//System.out.println(orderItems.get(0).toString());
-        List<Product> products=productRepository.findAll();
-        System.out.println(products.size());
-    }
+//		var temp = orderService.getOrdersThatRelatedToUser(7);
+//		System.out.println(temp);
+		
+	}
 }
--- src/main/java/ua/tony/service/UserService.java
+++ src/main/java/ua/tony/service/UserService.java
@@ -1,8 +1,60 @@
 package ua.tony.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import ua.tony.dto.ProductDto;
+import ua.tony.dto.UserDto;
+import ua.tony.entity.User;
+import ua.tony.mapper.UserMapper;
+import ua.tony.repository.UserRepository;
 
 @Service
 public class UserService {
 
+	@Autowired
+	UserMapper userMapper;
+	@Autowired
+	UserRepository userRepo;
+
+	public UserDto save(UserDto userDto) {
+
+		User user = userMapper.convertToEntity(userDto);
+		return userMapper.convertToDto(userRepo.save(user));
+	}
+
+	public UserDto update(UserDto userDto) {
+
+		User user = userMapper.convertToEntity(userDto);
+		return userMapper.convertToDto(userRepo.save(user));
+	}
+
+	public UserDto findById(Integer id) {
+
+		return userMapper.convertToDto(userRepo.findById(id).get());
+	}
+
+	public UserDto findByLogin(String login) {
+
+		return userMapper.convertToDto(userRepo.findByLogin(login));
+	}
+
+	public List<UserDto> findAll() {
+
+		List<UserDto> users = userRepo.findAll().stream().map(x -> userMapper.convertToDto(x)).toList();
+		return users;
+	}
+
+	public void deleteAll() {
+
+		userRepo.deleteAll();
+	}
+
+	public void deleteById(Integer id) {
+
+		userRepo.deleteById(id);
+	}
 }
